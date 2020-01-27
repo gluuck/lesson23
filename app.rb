e9e393cdc3643c0c5d38a79dc456d747446b3818
Vladimir Ostapenko <ostapenkov74@gmail.com>
2020-01-27 11:42:49 +0500

    change code app.rb

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School!!!</a>"
 end
 
 get '/about' do
@@ -12,4 +12,11 @@ end
 
 get '/visit' do
 	erb :visit
+end
+
+get '/contacts' do
+	erb :contacts
+end
+get '/admin' do
+	erb :admin
 end
